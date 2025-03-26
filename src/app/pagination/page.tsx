057cedad26d5d00066a49b6cd52b8d4b026b2586
Akashkoiri <akk27724@gmail.com>
2025-03-26 20:09:23 +0530

    Suspense Error Resolve for Deployment

--- src/app/pagination/page.tsx
+++ src/app/pagination/page.tsx
@@ -1,11 +1,14 @@
 import ArrayComp from "@/components/pagination/array-comp";
+import { Suspense } from "react";
 
 export default function Pagination() {
   return (
     <div className="h-150 w-xl mx-auto my-15 flex flex-col border border-red-600 ">
       <div className="text-4xl font-bold text-center p-5">Pagination</div>
       <div className="flex flex-col gap-4 p-4 pt-0 overflow-auto ">
-        <ArrayComp />
+        <Suspense>
+          <ArrayComp />
+        </Suspense>
       </div>
     </div>
   );
